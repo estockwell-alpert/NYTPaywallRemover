33d8e10105019f462444eb5e15dba7476c30966c
estockwell-alpert <erica.stockwell-alpert@velir.com>
2021-04-29 09:54:21 -0400

    Firefox extension - working, needs cleanup

---
+++ Firefox_Extension/content_scripts/beastify.js
@@ -0,0 +1,33 @@
+(function() {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+
+  /**
+   * Given a URL to a beast image, remove all existing beasts, then
+   * create and style an IMG node pointing to
+   * that image, then insert the node into the document.
+   */
+  function insertBeast(beastURL) {
+    document.getElementById("gateway-content").remove();
+    document.getElementsByClassName("css-mcm29f")[0].style.position = "initial";
+    document.getElementsByClassName("css-1bd8bfl")[0].style.background = "none";
+  }
+
+  /**
+   * Listen for messages from the background script.
+   * Call "beastify()" or "reset()".
+  */
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "beastify") {
+      insertBeast(message.beastURL);
+    }
+  });
+
+})();
